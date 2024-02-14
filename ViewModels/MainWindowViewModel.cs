6c1a1a7165d2cef378b41f745dca7513399d9a23
Michael Rusakov <mrusakov@gmail.com>
2024-02-14 19:21:29 +0100

    A tiny refactoring for better readability

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -13,111 +13,86 @@ using System.Reflection;
 using System.Text;
 using System.Xml.Linq;
 
-namespace Avalonia.TreeDataGrid.VariableRowsBug.ViewModels
+namespace Avalonia.TreeDataGrid.VariableRowsBug.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    private readonly Random _random = new ();
+    private readonly string[] _words;
+    private List<string> _randomPhrases = new List<string>();
+
+    public HierarchicalTreeDataGridSource<string> TreeWrap => new(_randomPhrases)
     {
-        private readonly Random _random = new ();
-        private readonly string[] _words;
-        private List<string> _randomWords = new List<string>();
-
-        public MainWindowViewModel()
+        Columns =
         {
-            try
-            {
-                _words = ReadAssetFile("1000-most-common-words.txt").Split(new[] { '\r', '\n' });
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);                
-            }
+            new HierarchicalExpanderColumn<string>
+            (
+                new TextColumn<string, string>("Random string", x => x, GridLength.Star, new(){ TextWrapping = TextWrapping.Wrap }),
+                x=> x.Split(' ')
+            ),
+            new TextColumn<string, string>("Words count", x => (x.Count(c => c == ' ') + 1).ToString(), GridLength.Auto, new (){ TextAlignment = TextAlignment.Center })
+        }
+    };
 
-            for (int i = 0; i < 100; i++)
-            {
-                _randomWords.Add(GetRandomString());
-            }
+
+    public HierarchicalTreeDataGridSource<string> TreeNoWrap => new(_randomPhrases)
+    {
+        Columns =
+        {
+            new HierarchicalExpanderColumn<string>
+            (
+                new TextColumn<string, string>("Random string", x => x, GridLength.Star, new(){ TextWrapping = TextWrapping.NoWrap }),
+                x=> x.Split(' ')
+            ), 
+            new TextColumn<string, string>("Words count", x => (x.Count(c => c == ' ') + 1).ToString(), GridLength.Auto, new (){ TextAlignment = TextAlignment.Center })
+        }
+    };
+
+    public MainWindowViewModel()
+    {
+        try
+        {
+            _words = ReadAssetFile("1000-most-common-words.txt").Split(new[] { '\r', '\n' });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
         }
 
-        private string GetRandomString(int wordCount = 0)
+        for (int i = 0; i < 100; i++)
         {
-            if (wordCount == 0)
-            { 
-                wordCount = _random.Next(1, 6); // 1 to 5 words (inclusive)
-            }
+            _randomPhrases.Add(GetRandomString());
+        }
+    }
 
-            var sb = new StringBuilder();
-            for (var i = 0; i < wordCount; ++i)
-            {
-                if (sb.Length > 0)
-                    sb.Append(" ");
-
-                sb.Append(_words[_random.Next(_words.Length)]);
-            }
-            return sb.ToString();
+    private string GetRandomString(int wordCount = 0)
+    {
+        if (wordCount == 0)
+        {
+            wordCount = _random.Next(1, 6); // 1 to 5 words (inclusive)
         }
 
-        public static string ReadAssetFile(string assetPath)
+        var sb = new StringBuilder();
+        for (var i = 0; i < wordCount; ++i)
         {
-            // Determine path
-            var assembly = Assembly.GetExecutingAssembly();
-            
+            if (sb.Length > 0)
+                sb.Append(" ");
 
-            using (Stream stream = assembly.GetManifestResourceStream("Avalonia.TreeDataGrid.VariableRowsBug.Assets." + assetPath))
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                return reader.ReadToEnd();
-            }
+            sb.Append(_words[_random.Next(_words.Length)]);
         }
+        return sb.ToString();
+    }
 
-        public HierarchicalTreeDataGridSource<string> TreeWrap => new(_randomWords)
+    public static string ReadAssetFile(string assetPath)
+    {
+        // Determine path
+        var assembly = Assembly.GetExecutingAssembly();
+
+
+        using (Stream stream = assembly.GetManifestResourceStream("Avalonia.TreeDataGrid.VariableRowsBug.Assets." + assetPath))
+        using (StreamReader reader = new StreamReader(stream))
         {
-            Columns =
-            {
-                new HierarchicalExpanderColumn<string>
-                (
-                    new TextColumn<string, string>("Random string", x => x, GridLength.Star, new(){ TextWrapping = TextWrapping.Wrap }),
-                    x=> x.Split(' ')
-                ),
-                new TextColumn<string, string>("Words count", x => (x.Count(c => c == ' ') + 1).ToString(), GridLength.Auto, new (){ TextAlignment = TextAlignment.Center })
-            }
-        };
-
-
-        public HierarchicalTreeDataGridSource<string> TreeNoWrap => new(_randomWords)
-        {
-            Columns =
-            {
-                new HierarchicalExpanderColumn<string>
-                (
-                    new TextColumn<string, string>("Random string", x => x, GridLength.Star, new(){ TextWrapping = TextWrapping.NoWrap }),
-                    x=> x.Split(' ')
-                ), 
-                new TextColumn<string, string>("Words count", x => (x.Count(c => c == ' ') + 1).ToString(), GridLength.Auto, new (){ TextAlignment = TextAlignment.Center })
-            }
-        };
-
-        //protected virtual ITreeDataGridSourceFileNode GetTreeData() => CreateTestTree();
-
-        //private static ITreeDataGridSourceFileNode CreateTestTree()
-        //{
-        //    var fileEntries = new Dictionary<RelativePath, int>
-        //{
-        //    { new RelativePath("Some very long file name that takes most of the screen and should show .bsa"), 1 },
-        //    { new RelativePath("BWS - Textures.bsa"), 2 },
-        //    { new RelativePath("Readme-BWS.txt"), 3 },
-        //    { new RelativePath("Textures/greenBlade.dds"), 4 },
-        //    { new RelativePath("Textures/greenBlade_n.dds"), 5 },
-        //    { new RelativePath("Textures/greenHilt.dds"), 6 },
-        //    { new RelativePath("Textures/Armors/greenArmor.dds"), 7 },
-        //    { new RelativePath("Textures/Armors/greenBlade.dds"), 8 },
-        //    { new RelativePath("Textures/Armors/greenHilt.dds"), 9 },
-        //    { new RelativePath("Meshes/greenBlade.nif"), 10 }
-        //};
-
-        //    var tree = FileTreeNode<RelativePath, int>.CreateTree(fileEntries);
-        //    return TreeDataGridSourceFileNode<RelativePath, int>.FromFileTree(tree);
-        //}
-
-
+            return reader.ReadToEnd();
+        }
     }
 }
